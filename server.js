b159530c73b0708663701184852708892c87d71d
gusun0 <fpatan@mail.ru>
2021-01-27 17:58:55 -0600

    cambios

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ if(process.env.NODE_ENV !== 'production'){
 
 
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 
 app.set('view engine', 'ejs');
@@ -15,6 +17,7 @@ app.set('views', __dirname + '/views');
 app.set('layout','layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false }));
 
 
 const mongoose = require('mongoose');
@@ -25,10 +28,12 @@ mongoose.connect(process.env.DATABASE_URL, {
 });
 
 const db = mongoose.connection;
+
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to mongoose'));
 
 
 app.use('/',indexRouter);
+app.use('/authors',authorRouter);
 
 app.listen(process.env.PORT || 3000);
